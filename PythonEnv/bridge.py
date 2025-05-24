288ead1b8e0e833f70c6d4cfdaf5c6973831af4b
Gary Guo <110691769+GGo0@users.noreply.github.com>
2025-05-24 01:16:58 -0400

    modularization

---
+++ PythonEnv/bridge.py
@@ -0,0 +1,95 @@
+import sys, pathlib, yaml, queue, threading, signal, time
+
+from sockets.admin_manager import AdminManager
+from sockets.socket_factory import create_unreal_socket
+
+from stable_baselines3.common.vec_env import DummyVecEnv, SubprocVecEnv
+
+from gym_wrappers.gym_wrapper_rl_base import GymWrapperRLBase
+from gym_wrappers.gym_wrapper_single_env import GymWrapperSingleEnv
+from gym_wrappers.gym_wrapper_multi_env import GymWrapperMultiEnv
+
+class AdminTHD(threading.Thread):
+
+    def __init__(self, ip, port, out_q):
+        super().__init__(daemon=True, name= "AdminThread")
+        self.ip, self.port = ip, port
+        self.q = out_q
+        self.admin = None
+
+    def run(self):
+        self.admin = AdminManager(ip=self.ip, port=self.port)
+        self.admin.connect()
+        env_type, obs_shape, act_shape, env_count = self.admin.wait_for_handshake()
+        self.q.put({
+            "env_type": env_type,
+            "obs_shape": obs_shape,
+            "act_shape": act_shape,
+            "env_count": env_count,
+            "admin": self.admin, 
+        })
+
+        while True:
+            try:
+                _ = self.admin.get_cmd_nonblocking()
+            except Exception:
+                time.sleep(0.3) #sleep for 2 seconds 
+                pass
+class envTHD:
+    def __init__(self, ip, port, meta):
+        self.ip, self.port = ip, port
+        self.meta = meta
+        self.obs_shape = meta["obs_shape"]
+        self.act_shape = meta["act_shape"]
+        self.env_type  = meta["env_type"]
+        self.n_envs    = meta["env_count"] if meta["env_type"] == "MULTI" else 1
+    
+    def init_single_env(self):
+        obs_shape, act_shape = self.obs_shape, self.act_shape
+        ip, port = self.ip, self.port
+        admin_sock           = self.meta["admin"].sock
+        env_type             = self.env_type
+
+        def  _init():
+            if env_type == "RLBASE":
+                print("[Training] RLBASE => reusing Admin socket")
+                return GymWrapperRLBase(
+                    sock=admin_sock,
+                    obs_shape=obs_shape,
+                    act_shape=act_shape,
+                )
+            else:  # SINGLE
+                print("[Training] SINGLE => new socket")
+                sock = create_unreal_socket(ip, port)
+                return GymWrapperSingleEnv(
+                    sock=sock,
+                    obs_shape=obs_shape,
+                    act_shape=act_shape,
+                )
+        return _init
+
+    def init_multi_env(self, idx):
+        ip, port          = self.ip, self.port
+        obs_shape         = self.obs_shape
+        act_shape         = self.act_shape
+
+        def _init():
+            print(f"[Training] MULTI => create {idx} sub-environments")
+            sock = create_unreal_socket(ip, port)
+            return GymWrapperMultiEnv(
+                sock=sock,
+                obs_shape=obs_shape,
+                act_shape=act_shape,
+                env_id=idx,
+            )
+        return _init
+
+    def build(self):
+        if self.env_type == "MULTI":
+            env_fns = [self.init_multi_env(i) for i in range(self.n_envs)]
+            return SubprocVecEnv(env_fns, start_method="spawn")
+        else:
+            env_fns = [self.init_single_env()]
+            return DummyVecEnv(env_fns)
+
+        
--- PythonEnv/train.py
+++ PythonEnv/train.py
@@ -1,12 +1,15 @@
 import argparse as ap
-import sys
-from sb3_trainer import sb3_trainer
+import sys, pathlib, queue, signal
+from bridge import AdminTHD, envTHD
+import utils as help
+
+from stable_baselines3.common.callbacks import CheckpointCallback
+
+ENV_IP  = "127.0.0.1"        # Unreal TCP server IP
+ENV_PORT = 7777         # Unreal TCP server port
 
 # -------------------- TRAINING SCRIPT -------------------- #
-
-if __name__ == "__main__":
-
-# ____Training Options ____ #
+def main():
     parser = ap.ArgumentParser()
     parser.add_argument("--config", type=str, help="Path to config file")
 
@@ -14,21 +17,52 @@ if __name__ == "__main__":
     if args.config is None:
         print("Path to config file not found.\n")
         sys.exit(1)
+    
+    cfg_file = pathlib.Path(args.config)
+    if not cfg_file.is_file():
+        print(f"Config file not found: {cfg_file}")
+        sys.exit(1)
 
-    sb3 = sb3_trainer(args.config)
+    algo, hps, paths, resume, total_timesteps, checkpt_freq = help.load_config(cfg_file)
 
-    # Initialize and connect the AdminManager then set up env
-    sb3.connect_bridge()
+    model_cls = help.get_model_class(algo)
+    print("HERE")
+    meta_q = queue.Queue(maxsize=1)
+    print("a")
+    master = AdminTHD(ENV_IP, ENV_PORT, meta_q)
+    print("b")
+    master.start()
 
-    # Create the model 
-    sb3.init_model()
+    print("HERE2")
+    meta_data = meta_q.get() #blocking for master
+    print("HERE3")
+    if meta_data["env_type"] is None or meta_data["obs_shape"] == 0 or meta_data["act_shape"] == 0:
+        print("[ERROR] Invalid handshake – aborting.")
+        sys.exit(1)
+    
+    vec_env = envTHD(ENV_IP, ENV_PORT, meta_data).build()
+
+    if resume:
+        print("\n =====================Continue training from previous checkpoint======================\n")
+        model = model_cls.load(paths["previous_model"], env=vec_env, force_reset=False, device=hps["device"])
+    else:
+        model = model_cls("MlpPolicy", vec_env, **hps)
+
+    # Periodic Imaging
+    imaging = CheckpointCallback(
+    save_freq=checkpt_freq,
+    save_path=paths["save_dir"],
+    name_prefix="checkpoint"
+    )
 
     # Train the model
-    sb3.train_model()
+    model.learn(total_timesteps=total_timesteps, reset_num_timesteps= not resume, callback=imaging)
+    model.save(path=paths["save_dir"])
+    print(f"[Training] Model saved to '{paths['save_dir']}'")
 
-    sb3.save_model()
+    meta_data["admin"].close()
+    vec_env.close()
 
-    # TODO: Notify Unreal that training is complete through admin connection:
-    # NEED TO ADD SEND COMMAND FOR ADMIN SOCKET, CURRENTLY USER JUST MANUALLY CLOSES UNREAL
     
-    sb3.disconnect_bridge()
+if __name__ == "__main__":
+    main()
